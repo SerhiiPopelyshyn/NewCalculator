584fb50cee504c3208ade883245dc2674f7fe380
Serhii Popelyshyn <serjukzol@gmail.com>
2020-06-01 16:29:26 +0100

    Initial commit

---
+++ src/Calculator/NumButton.java
@@ -0,0 +1,15 @@
+package Calculator;
+
+import javafx.scene.control.Button;
+
+public class NumButton extends Button {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
